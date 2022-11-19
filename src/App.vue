14e0a0a3de5f7983450ff090b6dbcc277c15bd84
JosephO1992 <josephraoreilly@gmail.com>
2022-11-19 12:43:08 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+	<div class="text-center bg-gray-800 h-screen w-screen text-slate-300 px-4">
+		<h1 class="h-36 flex items-center justify-center text-4xl font-bold">
+			Hello Cats
+		</h1>
+		<div class="grid grid-cols-5">
+			<div class="flex justify-center">
+				<div
+					class="
+						block
+						p-6
+						rounded-lg
+						bg-gray-700
+						shadow-lg shadow-slate-500
+						max-w-sm
+					"
+				>
+					<h5 class="text-xl leading-tight font-medium mb-2">Card title</h5>
+					<p class="text-base mb-4">
+						Some quick example text to build on the card title and make up the
+						bulk of the card's content.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
